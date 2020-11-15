0c562caa06a1e90f0e65976a73a19a4a06921d2e
Christopher Ramos <25787207+Cramos711@users.noreply.github.com>
2020-11-15 14:55:15 -0500

    bugfixing still

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,7 +6,7 @@ const isBrowser = typeof window !== "undefined";
 const auth = isBrowser
   ? new auth0.WebAuth({
       domain: process.env.GATSBY_AUTH0_DOMAIN,
-      clientID: process.env.AUTH0_CLIENTID,
+      clientID: process.env.GATSBY_AUTH0_CLIENTID,
       redirectUri: process.env.GATSBY_AUTH0_CALLBACK,
       responseType: "token id_token",
       scope: "openid profile email",
